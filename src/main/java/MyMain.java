475855d3cb44fe006ef36ccff6fcd06da030dc49
BrendanMcCulloch <22bmcculloch@athenian.org>
2020-09-16 16:28:26 +0000

    made module that converted binary to decimals format

--- src/main/java/MyMain.java
+++ src/main/java/MyMain.java
@@ -3,12 +3,31 @@ import java.util.Scanner;
 public class MyMain {
     
     public static int binaryToDecimal(String binary) { 
-        return -1;
-        // REPLACE WITH YOUR CODE HERE
+        int x = Integer.parseInt(binary);
+        int dec_value = 0; 
+  
+        int base = 1; 
+   
+        while (x > 0) { 
+            int last_digit = x % 10; 
+            x = x / 10; 
+  
+            dec_value += last_digit * base; 
+  
+            base = base * 2; 
+        } 
+  
+        return dec_value; 
     }
     
     
     public static void main(String[] args) {
-        // YOUR CODE HERE
+        Scanner scan = new Scanner(System.in);
+        System.out.print("binary number here");
+        String btod = scan.nextLine();
+        System.out.println("Your number in decimal form is: " );
+        
+        scan.close();
+
     }
 }
